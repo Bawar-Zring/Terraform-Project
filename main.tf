9880f4f0dcb2fe8aa2c45c8fe1b20e670186229f
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-01 21:43:27 +0300

    creating IGW, Route Table

--- main.tf
+++ main.tf
@@ -15,12 +15,18 @@ resource "aws_subnet" "public-AZ1" {
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+    tags = {
+        Name = "public-AZ1"
+    }
 }
 
 resource "aws_subnet" "private-AZ1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+    tags = {
+        Name = "private-AZ1"
+    }
 }
 
 resource "aws_subnet" "public-AZ2" {
@@ -28,10 +34,47 @@ resource "aws_subnet" "public-AZ2" {
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+    tags = {
+        Name = "public-AZ2"
+    }
 }
 
 resource "aws_subnet" "private-AZ2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1b"
+    tags = {
+        Name = "private-AZ2"
+    }
 }
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "IGW"
+    }  
+}
+
+resource "aws_route_table" "public-routes" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+  
+  tags = {
+    Name = "Public-Routes"
+  }
+}
+
+resource "aws_route_table_association" "public-AZ1" {
+  subnet_id      = aws_subnet.public-AZ1.id
+  route_table_id = aws_route_table.public-routes.id
+}
+
+resource "aws_route_table_association" "public-AZ2" {
+  subnet_id      = aws_subnet.public-AZ2.id
+  route_table_id = aws_route_table.public-routes.id
+}
+
