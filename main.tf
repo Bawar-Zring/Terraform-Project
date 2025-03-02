2ee96ad16e03185c43168cf9aba0c1a8e68801fa
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-02 06:48:50 +0300

    add depends_on

--- main.tf
+++ main.tf
@@ -160,12 +160,14 @@ resource "aws_lb_target_group_attachment" "proxy1_attach" {
   target_group_arn = aws_lb_target_group.proxy_tg.arn
   target_id        = aws_instance.proxy1.id
   port            = 80
+  depends_on      = [aws_instance.proxy1]
 }
 
 resource "aws_lb_target_group_attachment" "proxy2_attach" {
   target_group_arn = aws_lb_target_group.proxy_tg.arn
   target_id        = aws_instance.proxy2.id
   port            = 80
+  depends_on      = [aws_instance.proxy2]
 }
 
 resource "aws_lb_listener" "proxy_listener" {
