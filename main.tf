da8d205252b7555731ec2a602519c566aa3d4aad
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-02 07:31:23 +0300

    add private instances

--- main.tf
+++ main.tf
@@ -223,3 +223,98 @@ resource "aws_lb_listener" "proxy_listener" {
     target_group_arn = aws_lb_target_group.proxy_tg.arn
   }
 }
+
+resource "aws_instance" "backend1" {
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.private-AZ1.id
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+  tags = {
+    Name = "backend1"
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install -y nginx
+                echo "Backend 1" > /usr/share/nginx/html/index.html
+                sudo systemctl start nginx
+                sudo systemctl enable nginx
+              EOF
+}
+
+resource "aws_instance" "backend2" {
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.private-AZ2.id
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+  tags = {
+    Name = "backend2"
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install -y nginx
+                echo "Backend 2" > /usr/share/nginx/html/index.html
+                sudo systemctl start nginx
+                sudo systemctl enable nginx
+              EOF
+}
+
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "backend-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "instance"
+  depends_on = [aws_lb.proxy]
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "backend1_attach" {
+  target_group_arn = aws_lb_target_group.backend_tg.arn
+  target_id        = aws_instance.backend1.id
+  port            = 80
+  depends_on      = [aws_instance.backend1]
+}
+
+resource "aws_lb_target_group_attachment" "backend2_attach" {
+  target_group_arn = aws_lb_target_group.backend_tg.arn
+  target_id        = aws_instance.backend2.id
+  port            = 80
+  depends_on      = [aws_instance.backend2]
+}
+
+resource "aws_lb" "backend" {
+  name               = "backend"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.backend_sg.id]
+  subnets            = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "backend"
+  }
+  
+}
+
+resource "aws_lb_listener" "backend_listener" {
+  load_balancer_arn = aws_lb.backend.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
