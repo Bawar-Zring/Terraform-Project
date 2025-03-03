a8faf0f22aa200f508ed386379be1aa2abcd2c85
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-03 06:56:49 +0300

    fix

--- main.tf
+++ main.tf
@@ -200,7 +200,7 @@ resource "aws_lb_target_group" "proxy_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
   target_type = "instance"
-  depends_on = [aws_lb.proxy]
+  # depends_on = [aws_lb.proxy]
 
   health_check {
     path                = "/"
@@ -215,14 +215,14 @@ resource "aws_lb_target_group_attachment" "proxy1_attach" {
   target_group_arn = aws_lb_target_group.proxy_tg.arn
   target_id        = aws_instance.proxy1.id
   port            = 80
-  depends_on      = [aws_instance.proxy1]
+  # depends_on      = [aws_instance.proxy1]
 }
 
 resource "aws_lb_target_group_attachment" "proxy2_attach" {
   target_group_arn = aws_lb_target_group.proxy_tg.arn
   target_id        = aws_instance.proxy2.id
   port            = 80
-  depends_on      = [aws_instance.proxy2]
+  # depends_on      = [aws_instance.proxy2]
 }
 
 resource "aws_lb_listener" "proxy_listener" {
@@ -286,7 +286,7 @@ resource "aws_lb_target_group" "backend_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
   target_type = "instance"
-  depends_on = [aws_lb.backend]
+  # depends_on = [aws_lb.backend]
 
   health_check {
     path                = "/"
@@ -301,14 +301,14 @@ resource "aws_lb_target_group_attachment" "backend1_attach" {
   target_group_arn = aws_lb_target_group.backend_tg.arn
   target_id        = aws_instance.backend1.id
   port            = 80
-  depends_on      = [aws_instance.backend1]
+  # depends_on      = [aws_instance.backend1]
 }
 
 resource "aws_lb_target_group_attachment" "backend2_attach" {
   target_group_arn = aws_lb_target_group.backend_tg.arn
   target_id        = aws_instance.backend2.id
   port            = 80
-  depends_on      = [aws_instance.backend2]
+  # depends_on      = [aws_instance.backend2]
 }
 
 resource "aws_lb" "backend" {
