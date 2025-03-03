dba1891123a78659c9dcf5df451d053236f2ac6f
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-03 16:52:01 +0300

    fix

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_subnet" "private-AZ1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
     tags = {
         Name = "private-AZ1"
     }
@@ -43,6 +45,8 @@ resource "aws_subnet" "private-AZ2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
     tags = {
         Name = "private-AZ2"
     }
@@ -237,6 +241,7 @@ resource "aws_instance" "backend1" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private-AZ1.id
   vpc_security_group_ids = [aws_security_group.backend_sg.id, aws_security_group.proxy_sg.id]
+  associate_public_ip_address = true
   tags = {
     Name = "backend1"
   }
@@ -255,6 +260,7 @@ resource "aws_instance" "backend2" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private-AZ2.id
   vpc_security_group_ids = [aws_security_group.backend_sg.id, aws_security_group.proxy_sg.id]
+  associate_public_ip_address = true
   tags = {
     Name = "backend2"
   }
