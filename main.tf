712e796b1b2319e8561b7701565223218832b5b5
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-05 16:51:32 +0300

    remove VPC endpoint

--- main.tf
+++ main.tf
@@ -57,48 +57,6 @@ resource "aws_internet_gateway" "IGW" {
     }  
 }
 
-# resource "aws_route_table" "private-1" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.IGW.id
-#   }
-
-#   tags = {
-#     Name = "Private-1"
-#   }
-# }
-
-# resource "aws_route_table_association" "private-AZ1" {
-#   subnet_id      = aws_subnet.private-AZ1.id
-#   route_table_id = aws_route_table.private-1.id
-# }
-
-# resource "aws_route_table" "private-2" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.IGW.id
-#   }
-
-#   tags = {
-#     Name = "Private-2"
-#   }
-# }
-
-# resource "aws_route_table_association" "private-AZ2" {
-#   subnet_id      = aws_subnet.private-AZ2.id
-#   route_table_id = aws_route_table.private-2.id
-# }
-
-# resource "aws_vpc_endpoint" "s3" {
-#   vpc_id = aws_vpc.main.id
-#   service_name = "com.amazonaws.us-east-1.s3"
-#   route_table_ids = [aws_route_table.private-1.id, aws_route_table.private-2.id]  
-# }
-
 resource "aws_route_table" "public-routes" {
   vpc_id = aws_vpc.main.id
 
